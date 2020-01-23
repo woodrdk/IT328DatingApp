787133c351e72ff5db9cf7df7dce431a6099d89d
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-23 01:30:14 -0500

    added route info and htaccess

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 * This file routes the users at the index to the home page using FatFree Framework
 */
 
+// start a session
+session_start();
 // Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -21,6 +23,17 @@ $f3 -> route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
     //echo'<h1>Hello World!</h1>';
+});
+
+$f3 -> route('GET /order', function(){
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+$f3 -> route('POST /order2', function(){
+    // var_dump($_POST);
+    $_SESSION['food'] = $_POST['food'];
+    $view = new Template();
+    echo $view->render('views/form2.html');
 });
 // Run Fat Free
 $f3 -> run();
