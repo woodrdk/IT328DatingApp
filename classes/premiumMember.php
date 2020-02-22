985b13a8475b8fa302e8a535148028fe5097c4bc
Rob Wood <rwood3@mail.greenriver.edu>
2020-02-21 22:27:07 -0500

    premMember

---
+++ classes/premiumMember.php
@@ -0,0 +1,51 @@
+<?php
+
+class PremiumMember extends member
+{
+    private $inDoorInterests;
+    private $outDoorInterests;
+
+    /**
+     * PremiumMember constructor.
+     * @param $inDoorInterests
+     * @param $outDoorInterests
+     */
+    public function __construct($inDoorInterests, $outDoorInterests)
+    {
+        $this->inDoorInterests = $inDoorInterests;
+        $this->outDoorInterests = $outDoorInterests;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInDoorInterests()
+    {
+        return $this->inDoorInterests;
+    }
+
+    /**
+     * @param mixed $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->outDoorInterests;
+    }
+
+    /**
+     * @param mixed $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->outDoorInterests = $outDoorInterests;
+    }
+
+}
