1c0f4c8ca1ec2f27f53f0b557da4ca3bd362db96
Rob Wood <rwood3@mail.greenriver.edu>
2020-02-23 19:30:26 -0500

    added pic upload option buttons

--- upload.php
+++ upload.php
@@ -1,6 +1,33 @@
 <?php
 session_start();
-
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>Date By Food </title>
+    <!-- FAVICON -->
+    <link rel=\"icon\" type=\"image/ico\" href=\"images/food.ico\">
+    <!-- Bootstrap CSS -->
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">
+    <!-- Custom written CSS -->
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/style.css\">
+</head>
+<body>
+<nav class=\"navbar navbar-expand-lg navbar-dark\" style=\"background-color: red;\">
+    <a class=\"navbar-brand font-weight-bold\" href=\"home\">Date By Food</a>
+    <a class=\"navbar-brand\" href=\"home\">Home</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\"
+            data-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarNav\">
+        <ul class=\"navbar-nav\">
+            <li class=\"nav-item active\">
+                <a class=\"nav-link\" href=\"/\"> </a>
+            </li>
+        </ul>
+    </div>
+</nav>";
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -8,12 +35,9 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 echo "
-<include href='../views/header.html'>
     <div class='container redb rounded' id='container'>
         <h1>Upload Picture Status</h1>
         <hr>";
-
-
 
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -49,7 +73,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         $_SESSION['picName'] = basename( $_FILES["fileToUpload"]["name"]);
-        echo "<a href='summary'>Continue</a>";
+        echo " <button type=\"button\" class=\"btn text-center button\" onclick=\"window.location.href='summary'\">Continue</button>";
     }
     else {
         echo "Sorry, there was an error uploading your file.";
